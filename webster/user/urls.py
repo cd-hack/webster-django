c7254cdca760b4e85ee240966dc8844ab39fbab4
allenjiji <allenjiji2604@gmail.com>
2020-11-07 00:43:18 +0530

    completed rendering templates

--- webster/user/urls.py
+++ webster/user/urls.py
@@ -6,5 +6,11 @@ app_name = 'user'
 urlpatterns = [
     path('<storename>/', views.home, name='home'),
     path('<storename>/shop/', views.shop, name='shop'),
+    path('<storename>/about/', views.about, name='about'),
+    path('<storename>/contact/', views.contact, name='contact'),
+    path('<storename>/shop/<int:id>/', views.product_details, name='product-details'),
+    path('<storename>/wishlist/', views.wishlist, name='wishlist'),
+    path('<storename>/cart/', views.wishlist, name='cart'),
+    path('<storename>/checkout/', views.checkout, name='checkout'),
 
 ]
--- webster/user/views.py
+++ webster/user/views.py
@@ -9,3 +9,27 @@ def home(request, storename):
 
 def shop(request, storename):
     return render(request, 'user/shop.html')
+
+
+def about(request, storename):
+    return render(request, 'user/about.html')
+
+
+def contact(request, storename):
+    return render(request, 'user/contact.html')
+
+
+def product_details(request, storename, id):
+    return render(request, 'user/product-details.html')
+
+
+def wishlist(request, storename):
+    return render(request, 'user/wishlist.html')
+
+def cart(request, storename):
+    return render(request, 'user/cart.html')
+
+def checkout(request, storename):
+    return render(request, 'user/checkout.html')
+
+
